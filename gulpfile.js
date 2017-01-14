8768aef133e1468b84d59844fde9f6e7f9a15311
Paul Von Zimmerman <pcvonz@gmail.com>
2017-01-13 16:21:02 -0800

    Remove python stuff, completely using node now

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var nunjucks = require('gulp-nunjucks');
+var imagemin = require('gulp-imagemin');
+
+
+//TODO:
+//Implement the optimze functions from Zell's tutorial
+//https://css-tricks.com/gulp-for-beginners/
+
+gulp.task('nunjucks', function () {
+    gulp.src('templates/**.html')
+        .pipe(nunjucks.compile({name: 'Test'}))
+        .pipe(gulp.dest('public'))
+        .pipe(browserSync.reload({
+            stream: true
+            }))
+});
+
+
+//Function to optimize images (not really utilized)
+gulp.task('images', function() {
+    return gulp.src('source/images/**/*.+(png|jpg|gif|svg)')
+                                       .pipe(imagemin())
+                                       .pipe(gulp.dest('public/images'))
+
+});
+
+gulp.task('sass', function(){
+    return gulp.src('source/scss/style.scss')
+        .pipe(sass({
+            outputStyle: 'compressed',
+            includePaths: ['node_modules/susy/sass', 'node_modules/breakpoint-sass/stylesheets']
+        }))
+        .pipe(gulp.dest('public/css/'))
+        .pipe(browserSync.reload({
+            stream: true
+            }))
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'public'
+        }
+    })
+});
+
+//watch the scss folder and rukn sass whenever
+//a file changes
+//We put browser sync in an array as the second argument
+//that means that we want to run the browser sync task first
+//and then watch for file changers
+gulp.task('watch', ['browserSync', 'sass', 'images'], function() {
+    gulp.watch('source/scss/**/*.scss', ['sass']);
+    gulp.watch('templates/**/*.html', ['nunjucks']);
+    gulp.watch('source/js/**/*.js', browserSync.reload);
+    gulp.watch('source/images/**/*.+(png|jpg|gif|svg)', browserSync.reload);
+});
