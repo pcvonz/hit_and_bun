cf2b163deee74db76290c095d79ece3f9b4e6c95
Paul Von Zimmerman <pcvonz@gmail.com>
2017-01-13 17:02:55 -0800

    Adding hamburger menu

--- gulpfile.js
+++ gulpfile.js
@@ -26,13 +26,17 @@ gulp.task('images', function() {
                                        .pipe(gulp.dest('public/images'))
 
 });
+gulp.task('js', function() {
+    return gulp.src('source/js/**/*.js')
+                                       .pipe(gulp.dest('public/js'))
+});
 
 gulp.task('sass', function(){
-    return gulp.src('source/scss/style.scss')
+    return gulp.src('source/style.scss')
         .pipe(sass({
             outputStyle: 'compressed',
             includePaths: ['node_modules/susy/sass', 'node_modules/breakpoint-sass/stylesheets']
-        }))
+        }).on('error', sass.logError))
         .pipe(gulp.dest('public/css/'))
         .pipe(browserSync.reload({
             stream: true
@@ -52,7 +56,7 @@ gulp.task('browserSync', function() {
 //We put browser sync in an array as the second argument
 //that means that we want to run the browser sync task first
 //and then watch for file changers
-gulp.task('watch', ['browserSync', 'sass', 'images'], function() {
+gulp.task('watch', ['browserSync', 'sass', 'images', 'js'], function() {
     gulp.watch('source/scss/**/*.scss', ['sass']);
     gulp.watch('templates/**/*.html', ['nunjucks']);
     gulp.watch('source/js/**/*.js', browserSync.reload);
